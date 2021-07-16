f4ed08e4b671766793ab985fbc031873ef5c50ef
“sx1x1” <1105158282@qq.com>
2021-07-17 00:35:09 +0800

    基本搜索功能

---
+++ study-es/src/test/java/com/sxr/study/es/StudyEsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sxr.study.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
