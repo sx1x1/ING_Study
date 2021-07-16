cd27cb04067bb65c96dad6b4c12325e3e7b6192f
“sx1x1” <1105158282@qq.com>
2021-07-17 00:32:34 +0800

    基本搜索功能

--- study-es/src/main/java/com/sxr/study/es/api/Operation.java
+++ study-es/src/main/java/com/sxr/study/es/api/Operation.java
@@ -1,5 +1,6 @@
 package com.sxr.study.es.api;
 
+import lombok.SneakyThrows;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -9,35 +10,43 @@ import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 
-import java.io.IOException;
-
 /**
  * @author sxr
  * @date 2021/7/16 7:58 下午
  */
 public class Operation {
-    private static RestHighLevelClient client = ConnectEsUtil.getClient();
+    // 获取客户端
+    private static final RestHighLevelClient client = ConnectEsUtil.getClient();
 
-    public void query(){
+    // 查询
+    @SneakyThrows
+    public void query() {
+        // 搜索请求构建，设置请求路径
         SearchRequest request = new SearchRequest("movies");
 
+        // 请求体构建
         SearchSourceBuilder builder = new SearchSourceBuilder();
 
-        builder.query(QueryBuilders.termQuery("year",1962));
+        // 查询体构建
+        QueryBuilder queryBuilder = QueryBuilders.termQuery("year", 1962);
+        builder.query(queryBuilder);
 
+        // 搜索请求构建，设置请求体
         request.source(builder);
 
-        try {
-            SearchResponse response = client.search(request, RequestOptions.DEFAULT);
-            SearchHits hits = response.getHits();
-            hits.forEach(System.out::println);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        // 客户端查询
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+        // 获取返回结果
+        SearchHits hits = response.getHits();
+
+        // 输出
+        hits.forEach(System.out::println);
     }
 
+    @SneakyThrows
     public static void main(String[] args) {
-        final Operation operation = new Operation();
+        Operation operation = new Operation();
         operation.query();
     }
 }
