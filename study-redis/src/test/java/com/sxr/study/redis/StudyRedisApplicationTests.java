73363899504602a80d3a4231e8563bce12a4772c
“sx1x1” <1105158282@qq.com>
2021-08-05 17:24:51 +0800

    基本功能

---
+++ study-redis/src/test/java/com/sxr/study/redis/StudyRedisApplicationTests.java
@@ -0,0 +1,23 @@
+package com.sxr.study.redis;
+
+import com.sxr.study.redis.api.RedisApi;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+class StudyRedisApplicationTests {
+
+    @Resource
+    private RedisApi redisApi;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void stringApiTest(){
+        redisApi.stringApi();
+    }
+}
