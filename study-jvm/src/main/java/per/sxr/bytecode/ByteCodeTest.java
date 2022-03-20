d71bc526ad8b7fce4f9346996ee214e4dfc9de5e
“sx1x1” <1105158282@qq.com>
2022-03-20 21:42:15 +0800

    虚拟机

---
+++ study-jvm/src/main/java/per/sxr/bytecode/ByteCodeTest.java
@@ -0,0 +1,15 @@
+package per.sxr.bytecode;
+
+/**
+ * @author sxr
+ * @date 2022/3/20 12:02 下午
+ */
+public class ByteCodeTest {
+    public static void main(String[] args) {
+        int a = 9000;
+        boolean b = false;
+        byte c = 'q';
+        short s = 's';
+        char t = 't';
+    }
+}
